8abdfc81aefe316fbdea653c7ac706baecc7b748
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-06-04 13:31:11 +0200

    Increase version to 3.2 and cleanup test

--- tests/test_stock_quality_control_trigger_shipment_in.py
+++ tests/test_stock_quality_control_trigger_shipment_in.py
@@ -1,38 +1,22 @@
 #!/usr/bin/env python
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.backend.sqlite.database import Database as SQLiteDatabase
+from trytond.tests.test_tryton import test_depends, doctest_dropdb
 
 
 class TestCase(unittest.TestCase):
-    '''
-    Test module.
-    '''
+    'Test module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module(
             'stock_quality_control_trigger_shipment_in')
 
-
-def doctest_dropdb(test):
-    database = SQLiteDatabase().connect()
-    cursor = database.cursor(autocommit=True)
-    try:
-        database.drop(cursor, ':memory:')
-        cursor.commit()
-    finally:
-        cursor.close()
+    def test0006depends(self):
+        'Test depends'
+        test_depends()
 
 
 def suite():
@@ -43,6 +27,3 @@ def suite():
             setUp=doctest_dropdb, tearDown=doctest_dropdb, encoding='utf-8',
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
