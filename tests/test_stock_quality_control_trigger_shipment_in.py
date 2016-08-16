35b5c6fb853e83bcda2341b07461ca7c0be9cc5e
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-08-16 10:27:13 +0200

    Upgrade test

--- tests/test_stock_quality_control_trigger_shipment_in.py
+++ tests/test_stock_quality_control_trigger_shipment_in.py
@@ -4,20 +4,14 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module(
-            'stock_quality_control_trigger_shipment_in')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'stock_quality_control_trigger_shipment_in'
 
 
 def suite():
@@ -26,5 +20,6 @@ def suite():
     suite.addTests(doctest.DocFileSuite(
             'scenario_stock_quality_control_trigger_shipment_in.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
